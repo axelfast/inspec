68c2f9f204c0d72eeb55295026896f03fbd1dd27
Jared Quick <jquick@chef.io>
2017-12-14 13:08:57 -0500

    Testing train downcase platform names.
    Signed-off-by: Jared Quick <jquick@chef.io>

--- lib/resources/os.rb
+++ lib/resources/os.rb
@@ -27,15 +27,33 @@ module Inspec::Resources
       end
     end
 
+    # This is a string override for os.name.
+    # TODO: removed in inspec 2.0
+    class NameCleaned < String
+      def ==(other)
+        if other =~ /[A-Z ]/
+          cleaned = other.downcase.tr(' ', '_')
+          Inspec::Log.warn "[DEPRECATED] Platform names will become lowercase in InSpec 2.0. Please match on '#{cleaned}' instead of '#{other}'"
+        end
+        super(other)
+      end
+    end
+
     def [](name)
       # convert string to symbol
       name = name.to_sym if name.is_a? String
+      return self.name if name == :name
+
       inspec.backend.os[name]
+    end
+
+    def name
+      NameCleaned.new(inspec.backend.os[:name])
     end
 
     # add helper methods for easy access of properties
     # allows users to use os.name, os.family, os.release, os.arch
-    %w{name family release arch}.each do |property|
+    %w{family release arch}.each do |property|
       define_method(property.to_sym) do
         inspec.backend.os[property.to_sym]
       end
@@ -44,7 +62,7 @@ module Inspec::Resources
     # helper to collect a hash object easily
     def params
       {
-        name: inspec.backend.os[:name],
+        name: name,
         family: inspec.backend.os[:family],
         release: inspec.backend.os[:release],
         arch: inspec.backend.os[:arch],
